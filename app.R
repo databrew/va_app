da5fe6556c66c0de7b851b6c1efc06c4fc206b1a
benmbrew <benmbrew@gmail.com>
2019-07-18 16:28:51 -0400

    finished coders section, started reviewers

--- app.R
+++ app.R
@@ -3,51 +3,158 @@ source('global.R')
 # create header and define sidebar items
 header <- dashboardHeader(title="Databrew app")
 sidebar <- dashboardSidebar(
-    sidebarMenu(
-        menuItem(
-            text="Coders",
-            tabName="coders",
-            icon=icon("eye")),
-        menuItem(
-            text = 'Reviewers',
-            tabName = 'reviewers',
-            icon = icon("cog", lib = "glyphicon"))
-    )
+  sidebarMenu(
+    menuItem(
+      text="Coders",
+      tabName="coders",
+      icon=icon("eye")),
+    menuItem(
+      text = 'Reviewers',
+      tabName = 'reviewers',
+      icon = icon("cog", lib = "glyphicon"))
+  )
 )
 
 body <- dashboardBody(
-    tags$head(
-        tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
-    ),
-    tabItems(
-        tabItem(
-            tabName="coders",
-            fluidPage(
-                fluidRow(
-                    column(4,
-                           selectInput('doctor_name',
-                                       'Select user', 
-                                       choices = unique(coders$physician_name),
-                                       selected = unique(coders$physician_name[1]))),
-                    column(4,
-                           renderLeaflet('map')),
-                    column(4,
-                           selectInput('death_cert',
-                                       'Select Death certificate info',
-                                       choices = unique(coders$info_death_certificat)))# HERE
-                  
-                )
-            )
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  tabItems(
+    tabItem(
+      tabName="coders",
+      fluidPage(
+        fluidRow(
+          box( width = 12, 
+               title = '', 
+               status  = 'primary', 
+               solidHeader = T,
+          column(4,
+                 selectInput('doctor_name',
+                             'Select user', 
+                             choices = unique(coders$physician_name_1),
+                             selected =  unique(coders$physician_name_1)[1])),
+          column(4,
+                 leafletOutput('map')),
+          column(4,
+                 DT::dataTableOutput('death_cert_table')))
+          
         ),
-        tabItem(
-            tabName = 'reviewers',
-            fluidPage(
-                fluidRow(
-                    
-                )
-            )
+        fluidRow(
+          box( width = 4, 
+               title = 'Primary symptoms', 
+               status  = 'primary', 
+               solidHeader = T,
+          column(6,
+                 textOutput('symptoms'))),
+          box( width = 4, 
+               title = 'Narrative', 
+               status  = 'primary', 
+               solidHeader = T,
+          column(4,
+                 textOutput('narrative'))),
+          box( width = 4, 
+               title = 'Physician comments', 
+               status  = 'primary', 
+               solidHeader = T,
+          column(4,
+                 textOutput('comments')))# HERE
+          
+        ),
+        fluidRow(
+          box(width = 12,
+              title = 'Demographics & Cause of death',
+              status = 'primary',
+              solidHeader = T,
+          column(4,
+                 DT::dataTableOutput('demo_table')),
+         
+          column(2,
+                 selectInput('causa_directa',
+                             'Causa Directa', 
+                             choices = unique(coders$icd_directa),
+                             selected = unique(coders$icd_directa)[2])),
+          column(2,
+                
+                 selectInput('causa_intermedia',
+                             'Causa Intermedia', 
+                             choices = unique(coders$icd_intermedia),
+                             selected = unique(coders$icd_intermedia)[5])),
+          column(2,
+                 selectInput('causa_subjacente',
+                             'Causa Subjacente', 
+                             choices = unique(coders$icd_subjacente),
+                             selected = unique(coders$icd_subjacente)[3])))
         )
+      )
+    ),
+    tabItem(
+      tabName = 'reviewers',
+      fluidPage(
+        fluidRow(
+          box( width = 12, 
+               title = '', 
+               status  = 'primary', 
+               solidHeader = T,
+               column(4,
+                      selectInput('doctor_name_2',
+                                  'Select user', 
+                                  choices = unique(coders$physician_name_2),
+                                  selected =  unique(coders$physician_name_2)[1])),
+               column(4,
+                      leafletOutput('map_2')),
+               column(4,
+                      DT::dataTableOutput('death_cert_table_2')))
+          
+        ),
+        fluidRow(
+          box( width = 4, 
+               title = 'Primary symptoms', 
+               status  = 'primary', 
+               solidHeader = T,
+               column(6,
+                      textOutput('symptoms_2'))),
+          box( width = 4, 
+               title = 'Narrative', 
+               status  = 'primary', 
+               solidHeader = T,
+               column(4,
+                      textOutput('narrative_2'))),
+          box( width = 4, 
+               title = 'Physician comments', 
+               status  = 'primary', 
+               solidHeader = T,
+               column(4,
+                      textOutput('comments_2')))# HERE
+          
+        ),
+        fluidRow(
+          box(width = 12,
+              title = 'Demographics & Cause of death',
+              status = 'primary',
+              solidHeader = T,
+              column(4,
+                     DT::dataTableOutput('demo_table_2')),
+              
+              column(2,
+                     selectInput('causa_directa',
+                                 'Causa Directa', 
+                                 choices = unique(coders$icd_directa),
+                                 selected = unique(coders$icd_directa)[2])),
+              column(2,
+                     
+                     selectInput('causa_intermedia_2',
+                                 'Causa Intermedia', 
+                                 choices = unique(coders$icd_intermedia),
+                                 selected = unique(coders$icd_intermedia)[5])),
+              column(2,
+                     selectInput('causa_subjacente_2',
+                                 'Causa Subjacente', 
+                                 choices = unique(coders$icd_subjacente),
+                                 selected = unique(coders$icd_subjacente)[3])))
+        )
+      )
     )
+  )
 )
 
 # UI
@@ -55,8 +162,139 @@ ui <- dashboardPage(header, sidebar, body, skin="blue")
 
 # Server
 server <- function(input, output) {
+  
+  # create a reative data set that is subsetted by
+  
+  # create a uioutput for death cert details
+  doc_data <- reactive({
     
+    # get doctor name
+    doc_name <- input$doctor_name
+    
+    # subet data
+    x <- coders[coders$physician_name_1 == doc_name,]
+    
+    # change names of x
+    names(x) <- c('Physician name','Reviewing physician', 'Name', 'Gender', 'ID', 'Date of birth', 'Date of death', 'Place of death',
+                  'Lat', 'Lon', 'Age of death', 'Season of death', 'Symptoms', 'Description',
+                  'Causa imediata', 'Primeira causa', 'Comments', 'Causa directa', 'Causa intermedia', 'Cause subjacente')
+    
+    return(x)
+    
+  })
+  
+  # create a table output that shows the demographic details of deceased
+  output$demo_table <- DT::renderDataTable({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c('Name', 'Gender','ID', 'Date of birth', 'Date of death', 'Place of death', 'Age of death', 'Season of death')]
+    
+    # make data long
+    x <- as.data.frame(t(x))
+    
+    # replace colnames with blank
+    names(x) <- ''
+    
+    datatable(x, rownames = TRUE, colnames = '')
+    
+    
+  })
+  
+  # create a table output that shows the demographic details of deceased
+  output$demo_table <- DT::renderDataTable({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c('Name', 'Gender','ID', 'Date of birth', 'Date of death', 'Place of death', 'Age of death', 'Season of death')]
+    
+    # make data long
+    x <- as.data.frame(t(x))
+    
+    # replace colnames with blank
+    names(x) <- ''
+    
+    datatable(x, rownames = TRUE, colnames = '')
+    
+    
+  })
+  
+  # create a table output that shows the demographic details of deceased
+  output$death_cert_table <- DT::renderDataTable({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c( 'Causa imediata', 'Primeira causa')]
+    
+    # make data long
+    x <- as.data.frame(t(x))
+    
+    # replace colnames with blank
+    names(x) <- ''
+    
+    datatable(x, rownames = TRUE, colnames = '')
+    
+    
+  })
+  
+  # create a table output that shows the demographic details of deceased
+  output$narrative <-renderText({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c( 'Description')]
+    
+    paste0(as.character(x))
+    
+  })
+  
+  
+  # create a table output that shows the demographic details of deceased
+  output$comments <- renderText({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c( 'Comments')]
+    
+    paste0(as.character(x))
+    
+  })
+  
+  
+  # create a table output that shows the demographic details of deceased
+  output$symptoms <- renderText({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c( 'Symptoms')]
+    
+    paste0(as.character(x))
+    
+  })
+  
+  
+  # create a map output for leaflet
+  output$map <- renderLeaflet({
+    # get doc data from reactive object
+    x <- doc_data()
+    
+    # keep only name, id, gender, dob, dod, aod, and season
+    x <- x[, c('Name', 'Gender','ID', 'Date of birth', 'Date of death', 'Place of death', 'Age of death', 'Season of death', 'Lat', 'Lon')]
+    
+    leaflet(data = x) %>% addTiles() %>%
+      setView(x$Lat, x$Lon, zoom = 04) %>%
+      addMarkers(~Lat, ~Lon, popup = ~as.character(Name), label = ~as.character(Name))
+    
+  })
   
 }
+
+
 
 shinyApp(ui, server)
--- global.R
+++ global.R
@@ -16,16 +16,24 @@ library(shinyBS)
 library(aTSA)
 library(MASS)
 library(leaflet)
+library(reshape2)
+library(ggmap)
 
 # set options to take away scientific notation in graphs
 options(scipen=999)
 
+source('functions.R')
+
 # create a dataframe for users (physicians)
-coders <- data.frame(physician_name = c('Dr.Jones', 'Dr.Jones','Dr.Brown','Dr.Brown', 'Dr. Kelly', 'Dr. Kelly', 'Dr.Smith', 'Dr.Smith'),
-                     deceased_name = c('tim', 'james', 'laura', 'casey', 'karen', 'ben', 'sarah', 'edward'),
-                     id = c('1', '2', '3', '4', '5', '6', '7', '8'),
+coders <- data.frame(physician_name_1 = c('Dr.Jones', 'Dr.Claire','Dr.Brown','Dr.Lindsey', 'Dr. Kelly', 'Dr. George', 'Dr.Smith', 'Dr.Chiu'),
+                     physician_name_2 = c('Dr.Lionel', 'Dr.Lionel','Dr.Terrance','Dr.Terrance',
+                                          'Dr.Kim', 'Dr. Kim', 'Dr. Jay', 'Dr.Jay'),
+                     deceased_name = c('tim', 'tim', 'laura', 'laura', 'karen', 'karen', 'edward', 'edward'),
+                     deceased_gender = c('male', 'male', 'female', 'female', 'female', 'female', 'male', 'male'),
+                     id = c('1111', '1111', '2222', '2222', '3333', '3333', '4444', '4444'),
                      dob = c('01-14-1976','01-15-1977', '01-16-1978', '01-17-1979', '02-01-1968', '02-01-1969', '05-14-1991', '10-20-1965'),
                      dod = c('07-17-2012','08-15-2013', '11-01-1999', '03-02-2001', '07-29-1999', '01-20-2005', '01-01-2000', '12-27-2002'),
+                     pod= c('Region 1', 'Region 1', 'Region 2', 'Region 2', 'Region 3', 'Region 3', 'Region 4', 'Region 4'),
                      pod_lat = c(0,30, 20, 15, 10, 5, -10, -20),
                      pod_long = c(20, 0, -10, 20, 40, 20, 20, 30),
                      age_of_death = c(36, 36, 21, 22, 31, 36, 9, 37),
@@ -33,7 +41,8 @@ coders <- data.frame(physician_name = c('Dr.Jones', 'Dr.Jones','Dr.Brown','Dr.Br
                      symptoms = c('cough', 'diarrhea', 'diarrhea', 'nausea', 'fever', 'headache', 'abdominal_pain', 'profuse_sweating'),
                      description = c('heavy cough for 3 days', 'malnutrition', 'malnutrition', 'vomitting for several days', 'high fever of 103 F', 'persisting migraine', 
                      'sharp pain in abdomen when swallowing', 'over heated with high temperature and sweating at night'),
-                     info_death_certificat = c('10464/10465', '10466/10467','10464/10465', '10466/10467', '10468/10469', '10470/10471', '10472/10473', '10472/10473'),
+                     death_cert_causa_imediata = c('10464/10465', '10466/10467','10464/10465', '10466/10467', '10468/10469', '10470/10471', '10472/10473', '10472/10473'),
+                     death_cert_primeira_causa = c('13429/23042', '23242/11111','11321/10466', '40266/10400', '20468/40469', '10370/10999', '10432/10473', '10472/10684'),
                      comments = c('the individual showed serious symptoms of a pulminary infection', 
                      'watery stool in conjunction with fever and chills', 
                      'losing weight rapidly due to dysentery', 
@@ -42,14 +51,30 @@ coders <- data.frame(physician_name = c('Dr.Jones', 'Dr.Jones','Dr.Brown','Dr.Br
                      'sharp pain in forehead and persistent nausea', 
                      'abdominal pain and high fever',
                      'heavy sweating from pain and fever'),
-                     icd_list = c('I00-I99:Diseases of the circulatory system',
+                     icd_directa = c('I00-I99:Diseases of the circulatory system',
                                   'K00-K95:Diseases of the digestive system',
                                   'K00-K95:Diseases of the digestive system',
                                   'S00-T88:Injury, poisoning and certain other consequences of external causes', 
                                   'A00-B99:Certain infectious and parasitic diseases', 
                                   'F01-F99:Mental, Behavioral and Neurodevelopmental disorders',
                                   'A00-B99:Certain infectious and parasitic diseases',
-                                  'S00-T88:Injury, poisoning and certain other consequences of external causes'))
+                                  'S00-T88:Injury, poisoning and certain other consequences of external causes'),
+                     icd_intermedia= c('I00-I99:Diseases of the circulatory system',
+                                       'F01-F99:Mental, Behavioral and Neurodevelopmental disorders',
+                                     'K00-K95:Diseases of the digestive system',
+                                     'S00-T88:Injury, poisoning and certain other consequences of external causes', 
+                                     'A00-B99:Certain infectious and parasitic diseases', 
+                                     'A00-B99:Certain infectious and parasitic diseases',
+                                     'S00-T88:Injury, poisoning and certain other consequences of external causes',
+                                     'K00-K95:Diseases of the digestive system'),
+                     icd_subjacente = c('I00-I99:Diseases of the circulatory system',
+                                        'A00-B99:Certain infectious and parasitic diseases',
+                                     'K00-K95:Diseases of the digestive system',
+                                     'K00-K95:Diseases of the digestive system',
+                                     'S00-T88:Injury, poisoning and certain other consequences of external causes', 
+                                     'F01-F99:Mental, Behavioral and Neurodevelopmental disorders',
+                                     'A00-B99:Certain infectious and parasitic diseases',
+                                     'S00-T88:Injury, poisoning and certain other consequences of external causes'))
 
 # convert date variables to date object
 coders$dob <- as.Date(coders$dob, '%m-%d-%Y')
